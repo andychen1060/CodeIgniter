1d6232ca6ca0da1da9e96a8f3c353b23345b2e7b
Andy Chen <andychen0310@gmail.com>
2014-11-04 16:22:37 +0800

    Test Template Function
    Test template function

---
+++ application/controllers/Page.php
@@ -0,0 +1,24 @@
+﻿<?php
+class Page extends CI_Controller{
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('url');
+		$this->load->helper('form');  //載入Form輔助函數
+		$this->load->library('parser');		
+	}
+	
+	/**
+	index method
+	*/
+	public function index()
+	{		
+		$data = array(
+            'name' => 123,
+			'title' => '產學研後臺管理首頁'
+        );	
+		
+		$this->parser->parse('show_template', $data);
+	}
+}
